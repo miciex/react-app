8773aa974f7a688ee843e20ce86c1de3a094c3ca
miciex <maciejmilewski222@gmail.com>
2022-05-30 08:23:15 +0200

    change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Witaj</h1>
+      <button className="btn">Przycisk</button>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui vero
+        nesciunt, magni tenetur incidunt veritatis dolorum quod quae iure,
+        libero dicta saepe? Inventore libero suscipit odio ipsum magnam, dolor
+        error?
+      </p>
     </div>
   );
 }
